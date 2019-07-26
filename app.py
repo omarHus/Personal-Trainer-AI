32eba138abc4ca75674954752a98ccda3f8b26c8
Omar Husain <omar.husain2@gmail.com>
2019-07-26 13:36:14 -0400

    added html

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/upload_file')
+def upload_file():
+    return render_template('/upload_file.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
