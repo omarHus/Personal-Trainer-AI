f4c6cbae0ac8ad2f73289dd79487d1d6e4c8cfb7
Omar Husain <omar.husain2@gmail.com>
2019-07-26 13:43:26 -0400

    added run_test to app

--- app.py
+++ app.py
@@ -11,5 +11,14 @@ def index():
 def upload_file():
     return render_template('/upload_file.html')
 
+@app.route('/run_test', methods=['GET', 'POST'])
+def run_test():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(os.path.join(uploads_dir, secure_filename(file.filename)))
+        print(file.filename)
+        makeFrames(file.filename, uploads_dir)
+        return redirect('/results')
+
 if __name__ == '__main__':
     app.run(debug=True)
