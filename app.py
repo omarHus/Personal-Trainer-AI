66d1e7c8b3493670a67fafb2332788a755f63478
Omar Husain <omar.husain2@gmail.com>
2019-07-31 13:14:51 -0400

    heroku debugging for deploy

--- app.py
+++ app.py
@@ -37,31 +37,34 @@ def run_test():
     if request.method == 'POST':
         file = request.files['file']
         response  = upload(file, folder="squat_videos", resource_type = "video") #cloud upload
-        newFrames = tm2.makeFrames(response['secure_url']) #make image frames for predictions
-        print("**********************************************************************************")
-        print("**********************************************************************************")
-        print("Number of newFrames = ", newFrames)
-        print("**********************************************************************************")
-        print("**********************************************************************************")
+        print("Response = ", response['secure_url'])
+        if response:
+            newFrames = tm2.makeFrames(response['secure_url']) #make image frames for predictions
+            print("**********************************************************************************")
+            print("**********************************************************************************")
+            print("Number of newFrames = ", newFrames)
+            print("**********************************************************************************")
+            print("**********************************************************************************")
 
-        test_data  = tm2.processImages(newFrames)
-        orig_image = test_data[3]
-        test_image = test_data[2]
-        test_y     = test_data[1]
-        numTests   = test_data[0]
+            test_data  = tm2.processImages(newFrames)
+            orig_image = test_data[3]
+            test_image = test_data[2]
+            test_y     = test_data[1]
+            numTests   = test_data[0]
 
-        test_image = tm2.load_basemodel(test_image, numTests)
-        weights_path = get_file('trained_model.h5','https://github.com/omarHus/physioWebApp/raw/master/trained_model.h5')
-        model      = tm2.loadTrainedModel(weights_path)
+            test_image = tm2.load_basemodel(test_image, numTests)
+            weights_path = get_file('trained_model.h5','https://github.com/omarHus/physioWebApp/raw/master/trained_model.h5')
+            model      = tm2.loadTrainedModel(weights_path)
 
-        predictions = tm2.makepredictions(model, test_image)
-        goodSquats  = predictions[predictions==0].shape[0]
-        badSquats   = predictions[predictions==1].shape[0]
-        labeledImgs = tm2.createLabeledImages(orig_image, predictions)
-        movie       = tm2.videoOutput(labeledImgs,os.path.join(static_dir,'movie.gif'))
-        return render_template('/results.html', goodSquats=goodSquats, badSquats=badSquats, movie=movie) #sending data to html page to display
-    else:
-        return render_template('/upload_file.html')
+            predictions = tm2.makepredictions(model, test_image)
+            goodSquats  = predictions[predictions==0].shape[0]
+            badSquats   = predictions[predictions==1].shape[0]
+            labeledImgs = tm2.createLabeledImages(orig_image, predictions)
+            movie       = tm2.videoOutput(labeledImgs,os.path.join(static_dir,'movie.gif'))
+            return render_template('/results.html', goodSquats=goodSquats, badSquats=badSquats, movie=movie) #sending data to html page to display
+        else:
+            print("ERROR: Could not upload from cloud.")
+    return render_template('/upload_file.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
