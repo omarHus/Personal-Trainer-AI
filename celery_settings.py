e4972600736ca85c2a512ddc50e6dbca4ce1c5fb
Omar Husain <omar.husain2@gmail.com>
2019-08-10 14:52:07 -0400

    broker limit set to avoid max client reached in redis server

--- celery_settings.py
+++ celery_settings.py
@@ -4,3 +4,4 @@ CELERY_TASK_SERIALIZER = 'json'
 BROKER_URL             = os.environ.get('REDISTOGO_URL','redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT  = ['json']
 CELERY_RESULT_BACKEND  = os.environ.get('REDISTOGO_URL','redis://localhost:6379/0')
+BROKER_POOL_LIMIT      = 0
