15f66e6ab8f4bee0ada4440114544693999e6049
Omar Husain <omar.husain2@gmail.com>
2019-08-10 15:48:36 -0400

    trying to config celery to avoid max clients error

--- celery_settings.py
+++ celery_settings.py
@@ -1,8 +1,10 @@
 import os
 
-CELERY_TASK_SERIALIZER = 'json'
-BROKER_URL             = os.environ.get('REDISTOGO_URL','redis://localhost:6379/0')
-CELERY_ACCEPT_CONTENT  = ['json']
-CELERY_RESULT_BACKEND  = os.environ.get('REDISTOGO_URL','redis://localhost:6379/0')
-BROKER_POOL_LIMIT      = 0
-REDIS_MAX_CONNECTIONS  = 10
+CELERY_TASK_SERIALIZER       = 'json'
+BROKER_URL                   = os.environ.get('REDISTOGO_URL','redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT        = ['json']
+CELERY_RESULT_BACKEND        = os.environ.get('REDISTOGO_URL','redis://localhost:6379/0')
+BROKER_POOL_LIMIT            = None
+CELERY_REDIS_MAX_CONNECTIONS = 20
+CELERYD_WORKER_LOST_WAIT     = 20
+CELERYD_MAX_TASKS_PER_CHILD  = 6
