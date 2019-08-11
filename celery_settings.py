821269f72b61617c106bb1cc12d52dd86d716948
Omar Husain <omar.husain2@gmail.com>
2019-08-11 12:42:34 -0400

    changed redis server on heroku

--- celery_settings.py
+++ celery_settings.py
@@ -1,9 +1,9 @@
 import os
 
 CELERY_TASK_SERIALIZER       = 'json'
-BROKER_URL                   = os.environ.get('REDISTOGO_URL','redis://localhost:6379/0')
+BROKER_URL                   = os.environ.get('REDIS_URL','redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT        = ['json']
-CELERY_RESULT_BACKEND        = os.environ.get('REDISTOGO_URL','redis://localhost:6379/0')
+CELERY_RESULT_BACKEND        = os.environ.get('REDIS_URL','redis://localhost:6379/0')
 BROKER_POOL_LIMIT            = None
 CELERY_REDIS_MAX_CONNECTIONS = 20
 CELERYD_WORKER_LOST_WAIT     = 20
